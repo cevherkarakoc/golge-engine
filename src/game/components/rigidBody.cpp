556ff2e4b13a9a14b88ddcaf5d5f4463cbc91313
Cevher <cevherkarakoc@live.com>
2018-06-17 18:05:40 +0300

    ADD | Basic Shape to Rigid Body

--- src/game/components/rigidBody.cpp
+++ src/game/components/rigidBody.cpp
@@ -1,6 +1,7 @@
 #include <golge/game/components/rigidBody.h>
 
 #include "Box2D/Box2D.h"
+#include <glm/glm.hpp>
 
 #include <golge/game/components/transformComponent.h>
 
@@ -18,10 +19,18 @@ void RigidBody::init() {
 	bodyDef.position.Set(pos.x, pos.y);
 
 	m_body = m_entity->getScene()->getWorld()->CreateBody(&bodyDef);
+
+
+  b2PolygonShape shape;
+  shape.SetAsBox(0.05f, 0.05f);
+  m_body->CreateFixture(&shape,0.0f);
 }
 
 void RigidBody::update(float deltaTime) {
+  auto transform = m_entity->find<TransformComponent>("transform")->getTransform();
 
+	transform->setPosition(m_body->GetPosition().x, m_body->GetPosition().y);
+  transform->setRotation(glm::degrees( m_body->GetAngle() ));
 }
 
 std::string RigidBody::getName() const{
